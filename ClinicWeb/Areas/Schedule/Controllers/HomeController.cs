4905a74cef2cd62b6e3e661c7946da8c87639ea1
nekojuice <joseapple7@gmail.com>
2024-02-01 12:06:37 +0800

    完成所有Area，各含一個Index展示頁面

---
+++ ClinicWeb/Areas/Schedule/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ClinicWeb.Areas.Schedule.Controllers
+{
+	[Area("Schedule")]
+	public class HomeController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
