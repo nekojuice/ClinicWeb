927fc620f615a620c03c66e7965ff16ba83b5136
nekojuice <joseapple7@gmail.com>
2024-02-14 23:56:41 +0800

    嘗試用viewmodel製作表單

--- ClinicWeb/Areas/Member/Controllers/MemberController.cs
+++ ClinicWeb/Areas/Member/Controllers/MemberController.cs
@@ -3,233 +3,247 @@
 using ClinicWeb.Areas.Member.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using NuGet.Protocol;
 
 
 namespace ClinicWeb.Areas.Member.Controllers
 {
-    [Area("Member")]
-    public class MemberController : Controller
-    {
+	[Area("Member")]
+	public class MemberController : Controller
+	{
 
-        private ClinicSysContext _context;
-        public MemberController(ClinicSysContext context)
-        {
-            _context = context;
-        }
+		private ClinicSysContext _context;
+		public MemberController(ClinicSysContext context)
+		{
+			_context = context;
+		}
 
-        public IActionResult Index()
-        {
-            return View();
-        }
-        //老師建議：寫API可以拉一個獨立控制器
-        //[HttpGet] Member/MemberGetdata
-        //取得多筆會員資料
-        [Route("{area}/{controller}/{action}")]
-        public JsonResult MemberGetdata()
-        {
+		public IActionResult Index()
+		{
+			return View();
+		}
+		//老師建議：寫API可以拉一個獨立控制器
+		//[HttpGet] Member/MemberGetdata
+		//取得多筆會員資料
+		[Route("{area}/{controller}/{action}")]
+		public JsonResult MemberGetdata()
+		{
 
-            return Json(_context.MemberMemberList
-                .Select(x => new
-                {
-                    會員id = x.MemberId,
-                    會員編號 = x.MemberNumber,
-                    姓名 = x.Name,
-                    性別 = x.Gender ? "男" : "女",
-                    血型 = x.BloodType,
-                    身分證字號 = x.NationalId,
-                    聯絡電話 = x.Phone,
-                    地址 = x.Address,
-                    緊急聯絡人 = x.IceName,
-                    信箱 = x.MemEmail,
-
-
-                    啟用 = (bool)x.Verification ? "啟用" : "未啟用"
-                    //已經解決 性別不用這樣寫是為性別不允許null
-
-                }
-                ));
-        }
-
-        //修改完資料後抓取單筆資料
-        [Route("{area}/{controller}/{action}/{memberId}")]
-        [HttpPost]
-        public JsonResult MemberGetdataOne(int memberId)
-        {
-            return Json(_context.MemberMemberList
-                .Where(x => x.MemberId == Convert.ToInt32(memberId))
-                .Select(x => new
-                {
-                    會員id = x.MemberId,
-                    會員編號 = x.MemberNumber,
-                    姓名 = x.Name,
-                    性別 = x.Gender ? "男" : "女",
-                    血型 = x.BloodType,
-                    身分證字號 = x.NationalId,
-                    聯絡電話 = x.Phone,
-                    地址 = x.Address,
-                    緊急聯絡人 = x.IceName,
-                    信箱 = x.MemEmail,
+			return Json(_context.MemberMemberList
+				.Select(x => new
+				{
+					會員id = x.MemberId,
+					會員編號 = x.MemberNumber,
+					姓名 = x.Name,
+					性別 = x.Gender ? "男" : "女",
+					血型 = x.BloodType,
+					身分證字號 = x.NationalId,
+					聯絡電話 = x.Phone,
+					地址 = x.Address,
+					緊急聯絡人 = x.IceName,
+					信箱 = x.MemEmail,
 
 
-                    啟用 = (bool)x.Verification ? "啟用" : "未啟用"
-                })
-                .FirstOrDefault()
-                );
-        }
+					啟用 = (bool)x.Verification ? "啟用" : "未啟用"
+					//已經解決 性別不用這樣寫是為性別不允許null
 
-        //顯示多筆會員資料
-        public IActionResult MemIndex()
-        {
-            //可以指定不是這個名稱的view來顯示 return View("~Areas/Member/");
-            return View();
-        }
-        //新增會員資料
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public IActionResult MemberCreate([Bind("MemberNumber,Name,Gender,BloodType,NationalId,Address,ContactAddress,Phone,BirthDate,IceName,MemPassword,MemEmail,Verification,IsEnabled")] MemberMemberList member, string GenderString, string VerificationString)
-        {
-            //return Content("123");
-            //檢查驗證
-            if (!ModelState.IsValid)
-            {
-                // 如果模型驗證失敗，重新顯示包含錯誤信息的表單
-                return View(member); // 返回相同的視圖，這將會顯示錯誤信息
-            }
-            //加入資料庫
-            if (GenderString == "true")
-            {
-                member.Gender = true;
-            }
-            else { member.Gender = false; }
+				}
+				));
+		}
 
-            if (VerificationString == "on")
-            {
-                member.Verification = true;
-            }
-            else { member.Verification = false; }
-            var maxMemberNumber = _context.MemberMemberList.Max(m => m.MemberNumber);
-            var nextMemberNumber = maxMemberNumber + 1;
-            member.MemberNumber = nextMemberNumber;
-
-            _context.MemberMemberList.Add(member);
-            _context.SaveChanges();
-
-          
-            //    }
-            return View("~/Areas/Member/Views/MemIndex.cshtml");
-
-            
-
-        }
+		//修改完資料後抓取單筆資料
+		[Route("{area}/{controller}/{action}/{memberId}")]
+		[HttpPost]
+		public JsonResult MemberGetdataOne(int memberId)
+		{
+			return Json(_context.MemberMemberList
+				.Where(x => x.MemberId == Convert.ToInt32(memberId))
+				.Select(x => new
+				{
+					會員id = x.MemberId,
+					會員編號 = x.MemberNumber,
+					姓名 = x.Name,
+					性別 = x.Gender ? "男" : "女",
+					血型 = x.BloodType,
+					身分證字號 = x.NationalId,
+					聯絡電話 = x.Phone,
+					地址 = x.Address,
+					緊急聯絡人 = x.IceName,
+					信箱 = x.MemEmail,
 
 
+					啟用 = (bool)x.Verification ? "啟用" : "未啟用"
+				})
+				.FirstOrDefault()
+				);
+		}
 
-        //修改會員資料的畫面顯示
-        public async Task <IActionResult> MemberEdit(int? memberId)
-        {
-            if (memberId == null || _context.MemberMemberList == null)
-            {
-                return NotFound();
-            }
+		//顯示多筆會員資料
+		public IActionResult MemIndex()
+		{
+			//可以指定不是這個名稱的view來顯示 return View("~Areas/Member/");
+			return View();
+		}
+		//新增會員資料
+		/*[Bind("MemberNumber,Name,Gender,BloodType,NationalId,Address,ContactAddress,Phone,BirthDate,IceName,MemPassword,MemEmail,Verification,IsEnabled")]*/
+		/*, [FromBody] string GenderString, [FromBody] string VerificationString*/
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public IActionResult MemberCreate([FromBody] MemberViewModel member)
+		{
+			//測試用 驗證資料是否進入 是否為空值
+			if (member == null)
+			{
+				Console.WriteLine("null!!");
+			}
+			else
+			{
+				Console.WriteLine(member.ToJson());
+			}
 
-            var memberList = await _context.MemberMemberList.FindAsync(memberId);
-            if (memberList == null)
-            {
-                return NotFound();
-            }
-            return PartialView("~/Areas/Member/Views/Partial/_MemberEditPartial.cshtml", memberList);
-            //return View(memberEmployeeList);
+			//檢查驗證
+			if (!ModelState.IsValid || member == null)
+			{
+				// 如果模型驗證失敗，重新顯示包含錯誤信息的表單
+				return PartialView("~/Areas/Member/Views/Partial/_MemberCreatePartial.cshtml", member);
+			}
+			//加入資料庫
+			int maxMemberNumber = _context.MemberMemberList.Max(m => m.MemberNumber);
+			var nextMemberNumber = maxMemberNumber + 1;
 
-            //var member = _context.MemberMemberList.Where(m => m.MemberId == memberId).FirstOrDefault();
+			_context.MemberMemberList.Add(new MemberMemberList
+			{
+				MemberNumber = nextMemberNumber,
+				Name = member.Name,
+				NationalId = member.NationalId,
+				MemEmail = member.MemEmail,
+				Phone = member.Phone,
+				Gender =member.Gender,
+				BloodType = member.BloodType,
+				Address = member.Address,
+				ContactAddress = member.ContactAddress,
+				IceName = member.IceName,
+				IceNumber = member.IceNumber,
+				MemPassword = member.MemPassword,
+				Verification = member.Verification,
+				BirthDate = (DateTime)member.BirthDate
+			});
+			_context.SaveChanges();
 
-            //if (member == null) { return Content("123"); } 
-            //MemberViewModel memVM = new MemberViewModel
-            //{
-            //    // 將原始模型的屬性賦值給 ViewModel
-            //    MemberIdVW = member.MemberId,
-            //    MemberNumberVW = member.MemberNumber,
-            //    NameVW = member.Name,
-            //    GenderVW = member.Gender,
-            //    BloodTypeVW = member.BloodType,
-            //    NationalIdVW = member.NationalId,
-            //    AddressVW = member.Address,
-            //    ContactAddressVW = member.ContactAddress,
-            //    PhoneVW = member.Phone,
-            //    BirthDateVW = member.BirthDate,
-            //    IceNameVW = member.IceName,
-            //    MemPasswordVW = member.MemPassword,
-            //    MemEmailVW = member.MemEmail,
-            //    VerificationVW = member.Verification,
-
-
-            //};
-            //return PartialView("~/Areas/Member/Views/Partial/_MemberEditPartial.cshtml", member);
-
-        }
-
-
-
-
-        //編輯資料時檢查該筆會員id是否存在的函數
-        private bool MemberMemberListExists(int memberId)
-        {
-            return _context.MemberMemberList.Any(e => e.MemberId == memberId);
-        }
+			return Content("Success");
+		}
+		public IActionResult MemberCreatePartial()
+		{
+			return PartialView("~/Areas/Member/Views/Partial/_MemberCreatePartial.cshtml");
+		}
 
 
-        //把會員編輯好的資料送回資料庫
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-/* [Bind("MemberId,MemberNumber,Name,Gender,BloodType,NationalId,Address,ContactAddress,Phone,BirthDate,IceName,MemPassword,MemEmail,Verification,IsEnabled")]*/ 
-        public async Task<IActionResult> Edit([Bind("MemberId,MemberNumber,Name")]int MemberId,MemberMemberList member, string GenderString, string VerificationString)
-        {
-            if (MemberId != member.MemberId)
-            {
-                return NotFound();
-            }
+		//修改會員資料的畫面顯示
+		public async Task<IActionResult> MemberEdit(int? memberId)
+		{
+			if (memberId == null || _context.MemberMemberList == null)
+			{
+				return NotFound();
+			}
 
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    if (GenderString == "true")
-                    {
-                        member.Gender = true;
-                    }
-                    else { member.Gender = false; }
+			var memberList = await _context.MemberMemberList.FindAsync(memberId);
+			if (memberList == null)
+			{
+				return NotFound();
+			}
+			return PartialView("~/Areas/Member/Views/Partial/_MemberEditPartial.cshtml", memberList);
+			//return View(memberEmployeeList);
 
-                    if (VerificationString == "on")
-                    {
-                        member.Verification = true;
-                    }
-                    else { member.Verification = false; }
+			//var member = _context.MemberMemberList.Where(m => m.MemberId == memberId).FirstOrDefault();
 
-                    _context.Update(member);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!MemberMemberListExists(member.MemberId))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                //return RedirectToAction(nameof(Index));
-            }
-            else 
-            {
-                return  Content("驗證未通過");
-            }
-            
-            // 取得剛剛那筆會員資料的 ID
-            int memberId = member.MemberId;
-            //return View("~/Areas/Member/Views/_MemberIndex.cshtml",  member.MemberId);
-            return MemberGetdataOne(memberId);
-        }
+			//if (member == null) { return Content("123"); } 
+			//MemberViewModel memVM = new MemberViewModel
+			//{
+			//    // 將原始模型的屬性賦值給 ViewModel
+			//    MemberIdVW = member.MemberId,
+			//    MemberNumberVW = member.MemberNumber,
+			//    NameVW = member.Name,
+			//    GenderVW = member.Gender,
+			//    BloodTypeVW = member.BloodType,
+			//    NationalIdVW = member.NationalId,
+			//    AddressVW = member.Address,
+			//    ContactAddressVW = member.ContactAddress,
+			//    PhoneVW = member.Phone,
+			//    BirthDateVW = member.BirthDate,
+			//    IceNameVW = member.IceName,
+			//    MemPasswordVW = member.MemPassword,
+			//    MemEmailVW = member.MemEmail,
+			//    VerificationVW = member.Verification,
 
-    }
+
+			//};
+			//return PartialView("~/Areas/Member/Views/Partial/_MemberEditPartial.cshtml", member);
+
+		}
+
+
+
+
+		//編輯資料時檢查該筆會員id是否存在的函數
+		private bool MemberMemberListExists(int memberId)
+		{
+			return _context.MemberMemberList.Any(e => e.MemberId == memberId);
+		}
+
+
+		//把會員編輯好的資料送回資料庫
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		/* [Bind("MemberId,MemberNumber,Name,Gender,BloodType,NationalId,Address,ContactAddress,Phone,BirthDate,IceName,MemPassword,MemEmail,Verification,IsEnabled")]*/
+		public async Task<IActionResult> Edit([Bind("MemberId,MemberNumber,Name")] int MemberId, MemberMemberList member, string GenderString, string VerificationString)
+		{
+			if (MemberId != member.MemberId)
+			{
+				return NotFound();
+			}
+
+			if (ModelState.IsValid)
+			{
+				try
+				{
+					if (GenderString == "true")
+					{
+						member.Gender = true;
+					}
+					else { member.Gender = false; }
+
+					if (VerificationString == "on")
+					{
+						member.Verification = true;
+					}
+					else { member.Verification = false; }
+
+					_context.Update(member);
+					await _context.SaveChangesAsync();
+				}
+				catch (DbUpdateConcurrencyException)
+				{
+					if (!MemberMemberListExists(member.MemberId))
+					{
+						return NotFound();
+					}
+					else
+					{
+						throw;
+					}
+				}
+				//return RedirectToAction(nameof(Index));
+			}
+			else
+			{
+				return Content("驗證未通過");
+			}
+
+			// 取得剛剛那筆會員資料的 ID
+			int memberId = member.MemberId;
+			//return View("~/Areas/Member/Views/_MemberIndex.cshtml",  member.MemberId);
+			return MemberGetdataOne(memberId);
+		}
+
+	}
 }
--- ClinicWeb/Areas/Member/ViewModels/MemberViewModel.cs
+++ ClinicWeb/Areas/Member/ViewModels/MemberViewModel.cs
@@ -1,101 +1,121 @@
 ﻿using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
-using Microsoft.AspNetCore.Mvc;
 
 namespace ClinicWeb.Areas.Member.ViewModels
 {
-    [Area ("Member")]
-    public class Membermetadata
-    {
-        [Key]
-        [Column("Member_ID")]
-        public int MemberIdVW { get; set; }
+	public class MemberViewModel
+	{
+		[Key]
+		[Column("Member_ID")]
+		public int MemberId { get; set; }
 
-        //[Column("Member_Number")]
-        [Display(Name = "會員編號")]
-        public int MemberNumberVW { get; set; }
+		//[Column("Member_Number")]
+		[Display(Name = "會員編號")]
+		public int MemberNumber { get; set; }
 
-        //[Required]
-        //[StringLength(50)]
-        [Display(Name = "姓名")]
-        public string ?NameVW { get; set; }
-
-
-
-        //private bool _gender;
-        [Display(Name = "性別")]
-        public bool GenderVW{ get; set; }
-        public string ?GenderString {  get; set; }
-
-        //為了接前台傳來的字串
-        //{
-
-        //    //get { return _gender; }
-        //    //set { _gender = value; }
-        //}
-        //public bool GenderVW{ get; set; }
+		//[Required]
+		//[StringLength(50)]
+		[Display(Name = "姓名")]
+		[Required(ErrorMessage = "請輸入姓名")]
+		public string? Name { get; set; }
 
 
 
+		//private bool _gender;
+		[Display(Name = "性別")]
+		public bool Gender { get; set; }
+
+		[MinLength(4, ErrorMessage = "性別為必填")]
+		[Required(ErrorMessage = "性別為必填")]
+		public string? GenderString //{ get; set; }
+		{
+			get { return _gender; }
+			set
+			{
+				_gender = value;
+				try
+				{
+					Gender = Convert.ToBoolean(value);
+				}
+				catch (Exception)
+				{
+				};
+			}
+		}
+		public string? _gender { get; set; }
 
 
+		//[Required]
+		//[Column("Blood_Type")]
+		//[StringLength(50)]
+		[Display(Name = "血型")]
+		public string? BloodType { get; set; }
 
-        //[Required]
-        //[Column("Blood_Type")]
-        //[StringLength(50)]
-        [Display(Name = "血型")]
-        public string ?BloodTypeVW { get; set; }
+		//[Required]
+		//[Column("National_ID")]
+		//[StringLength(50)]
+		[Display(Name = "身分證字號")]
+		public string? NationalId { get; set; }
 
-        //[Required]
-        //[Column("National_ID")]
-        //[StringLength(50)]
-        [Display(Name = "身分證字號")]
-        public string ?NationalIdVW { get; set; }
+		//[Required]
+		//[StringLength(50)]
+		[Display(Name = "戶籍地址")]
+		public string? Address { get; set; }
 
-        //[Required]
-        //[StringLength(50)]
-        [Display(Name = "戶籍地址")]
-        public string ?AddressVW { get; set; }
+		//[Required]
+		//[Column("Contact_Address")]
+		//[StringLength(50)]
+		[Display(Name = "聯絡地址")]
+		public string? ContactAddress { get; set; }
 
-        //[Required]
-        //[Column("Contact_Address")]
-        //[StringLength(50)]
-        [Display(Name = "聯絡地址")]
-        public string ?ContactAddressVW { get; set; }
+		//[Required]
+		//[StringLength(50)]
+		[Display(Name = "連絡電話")]
+		public string? Phone { get; set; }
 
-        //[Required]
-        //[StringLength(50)]
-        [Display(Name = "連絡電話")]
-        public string ?PhoneVW { get; set; }
+		//[Column("Birth_Date", TypeName = "datetime")]
+		//[Required(ErrorMessage = "請選擇生日 ")]
+		[Display(Name = "生日")]
+		public DateTime? BirthDate { get; set; }
 
-        //[Column("Birth_Date", TypeName = "datetime")]
-        [Display(Name = "生日")]
-        public DateTime BirthDateVW { get; set; }
+		//[Column("ICE_Name")]
+		//[StringLength(50)]
+		[Display(Name = "緊急聯絡人")]
+		public string? IceName { get; set; }
 
-        //[Column("ICE_Name")]
-        //[StringLength(50)]
-        [Display(Name = "緊急聯絡人")]
-        public string ?IceNameVW { get; set; }
+		//[Column("ICE_Number")]
+		//[StringLength(50)]
+		[Display(Name = "緊急聯絡人電話")]
+		public string? IceNumber { get; set; }
 
-        //[Column("ICE_Number")]
-        //[StringLength(50)]
-        [Display(Name = "緊急聯絡人電話")]
-        public string ?IceNumberVW { get; set; }
+		//[Column("Mem_Password")]
+		//[StringLength(50)]
+		[Required(ErrorMessage = "必須至少為8碼，包含大小寫英文字、數字、特殊符號")]
+		[Display(Name = "密碼")]
+		public string? MemPassword { get; set; }
 
-        //[Column("Mem_Password")]
-        //[StringLength(50)]
-        [Display(Name = "密碼")]
-        public string ?MemPasswordVW { get; set; }
-
-        //[Column("Mem_Email")]
-        //[StringLength(50)]
-        [Display(Name = "信箱")]
-        public string ?MemEmailVW { get; set; }
+		//[Column("Mem_Email")]
+		//[StringLength(50)]
+		[Required(ErrorMessage = "請輸入信箱")]
+		[Display(Name = "信箱")]
+		public string? MemEmail { get; set; }
 
 
-        [Display(Name = "啟用")]
-        public bool? VerificationVW { get; set; }
-        public string? VerificationString { get; set; }
-       
-    }
+		[Display(Name = "啟用")]
+		public bool? Verification { get; set; }
+		public string? VerificationString
+		{
+			get
+			{
+				return _verificationString;
+			}
+			set
+			{
+				_verificationString = value;
+				Verification = value.Equals("on") ? true : false;
+			}
+		}
+		public string? _verificationString { get; set; }
+
+	}
 }
