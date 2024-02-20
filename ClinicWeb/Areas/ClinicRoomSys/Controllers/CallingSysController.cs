e7a0af4eb2cdb6b2e5a5184e202ec4a54de4cdc2
nekojuice <joseapple7@gmail.com>
2024-02-20 16:28:02 +0800

    叫號機雛形完成進度

--- ClinicWeb/Areas/ClinicRoomSys/Controllers/CallingSysController.cs
+++ ClinicWeb/Areas/ClinicRoomSys/Controllers/CallingSysController.cs
@@ -1,4 +1,5 @@
-﻿using ClinicWeb.Areas.ClinicRoomSys.Models.ViewModels;
+﻿using ClinicWeb.Areas.Appointment.Models;
+using ClinicWeb.Areas.ClinicRoomSys.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ClinicWeb.Areas.ClinicRoomSys.Controllers
@@ -9,15 +10,32 @@ namespace ClinicWeb.Areas.ClinicRoomSys.Controllers
     [Area("ClinicRoomSys")]
     public class CallingSysController : Controller
     {
+        private readonly ClinicSysContext _context;
+        public CallingSysController(ClinicSysContext context)
+        {
+            _context = context;
+        }
+
         //testdata docId=4 20240201 中午
         [HttpPost]
         [Route("{area}/{controller}/{action}")]
         public IActionResult Get_CallingList([FromBody] CallingPanelViewModel vm)
         {
-            Console.WriteLine(vm.doctorId.ToString());
-            Console.WriteLine(vm.date);
-            Console.WriteLine(vm.shiftId.ToString());
-            return Json("");
+            //[debug] 擷取傳入的值
+            //Console.WriteLine($"doctor: { vm.doctorId}, date: {vm.date}, shiftId: {vm.shiftId}");
+            return Json(_context.ApptClinicList
+                .Where(x => x.Clinic.DoctorId == vm.doctorId
+                && x.Clinic.Date == vm.date
+                && x.Clinic.ClinicTimeId == vm.shiftId)
+                .Select(x=>new {
+					member_id = x.MemberId,
+					status_id = x.PatientStateId,
+					診號 = x.ClinicNumber,
+					姓名 = x.Member.Name,
+					性別 = x.Member.Gender? "男":"女",
+					狀態 = x.PatientState.PatientStateName
+				})
+                );
         }
     }
 }
