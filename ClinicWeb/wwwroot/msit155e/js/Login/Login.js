780c140afb746b40b41b1ba91c9014277ed59017
Vcie <vcie2210@gmail.com>
2024-02-22 23:48:50 +0800

    可以讀取員工大頭照

---
+++ ClinicWeb/wwwroot/msit155e/js/Login/Login.js
@@ -0,0 +1,20 @@
+﻿(async () => {
+    const response = await fetch(`/Employee/Main/Profile`);
+    const data = await response.json();
+
+    document.getElementById('EmpType').innerText = data.EmpType;
+    document.getElementById('staffname').innerText = data.StaffName;
+    console.log(document.cookie);
+
+})();
+
+(async () => {
+    const response = await fetch(`/Employee/Main/ProfileForPicture`);
+    if (response.ok) {
+        const blob = await response.blob();
+        const imgUrl = URL.createObjectURL(blob);
+        document.getElementById('profilePic').src = imgUrl;
+    } else {
+        console.error('抓大頭照的api怪怪的');
+    }
+})();
