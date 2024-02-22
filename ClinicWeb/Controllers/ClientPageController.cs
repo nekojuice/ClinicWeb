32b178bff9609687de271451ed1d7ca9e947475f
NickChen <y42146988@gmail.com>
2024-02-22 14:59:05 +0800

    先有畫面。

---
+++ ClinicWeb/Controllers/ClientPageController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ClinicWeb.Controllers
+{
+	public class ClientPageController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+		public IActionResult services()
+		{
+			return View();
+		}
+	}
+}
