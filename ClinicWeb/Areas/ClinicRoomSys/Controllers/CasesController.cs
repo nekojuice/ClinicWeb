d9c18d34c41865c052266be9f0581d69321c7b45
NickInTheSandBox <154652330+NickInTheSandBox@users.noreply.github.com>
2024-02-24 16:34:40 +0800

    Api Update.

--- ClinicWeb/Areas/ClinicRoomSys/Controllers/CasesController.cs
+++ ClinicWeb/Areas/ClinicRoomSys/Controllers/CasesController.cs
@@ -1,13 +1,30 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ClinicWeb.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ClinicWeb.Areas.ClinicRoomSys.Controllers
 {
     [Area("ClinicRoomSys")]
+    
     public class CasesController : Controller
     {
+
+        private readonly ClinicSysContext _context;
+        public CasesController(ClinicSysContext context)
+        {
+            _context = context;
+        }
         //public IActionResult Index()
         //{
         //    return View();
         //}
+        [HttpPost]
+        [Route("{area}/{controller}/{action}/{ID}")]
+        public JsonResult MSID(string ID)
+        {
+            return Json(_context.CasesMainCase.Where(x=>x.MemberId.ToString() == ID) );
+        }
+        //[HttpPut]
+        //[HttpDelete]
     }
+  
 }
