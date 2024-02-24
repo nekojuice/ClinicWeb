3bc5605860c25780e4ed14bca69c95790711b341
Hakurou0116 <jojo41525@gmail.com>
2024-02-24 22:20:57 +0800

    0224 醫師班表排班按鈕有誤

---
+++ ClinicWeb/Areas/Schedule/Controllers/ClinicInfoController.cs
@@ -0,0 +1,18 @@
+﻿using ClinicWeb.Areas.Schedule.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ClinicWeb.Areas.Schedule.Controllers
+{
+    [Area("Schedule")]//路由設定123
+    public class ClinicInfoController : Controller
+    {
+        private readonly ClinicSysContext _context;
+        public ClinicInfoController(ClinicSysContext context)
+        {
+            _context = context;
+
+        }
+
+
+    }
+}
