0c0051b0fc2820b0fdf3f51e9e244890b4941071
nekojuice <joseapple7@gmail.com>
2024-02-29 09:07:42 +0800

    更新叫號器前台樣式、新增前台網頁區塊

---
+++ ClinicWeb/Hubs/CallingHub.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace ClinicWeb.Hubs
+{
+    public class CallingHub : Hub
+    {
+        //public async Task<string> WaitForMessage(string connectionId)
+        //{
+        //    var message = await Clients.Client(connectionId).InvokeAsync<string>(
+        //        "GetMessage");
+        //    return message;
+        //}
+    }
+}
