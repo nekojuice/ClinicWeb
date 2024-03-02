cbe3f14c78301707154b8bd648aa4798e2585502
NickChen <y42146988@gmail.com>
2024-03-02 11:55:38 +0800

    會員後台初版。

---
+++ ClinicWeb/Controllers/FMemberBController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ClinicWeb.Controllers
+{
+    public class FMemberBController : Controller
+    {
+        public IActionResult MemberIndex()
+        {
+            return View();
+        }
+    }
+}
