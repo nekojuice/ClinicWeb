c2823c7254c0e2d3626378931b6e41b4a8642893
nekojuice <joseapple7@gmail.com>
2024-03-05 00:40:33 +0800

    更新掛號查詢樣式

---
+++ ClinicWeb/wwwroot/msit155e/js/AppointmentSearch/apptSearch.js
@@ -0,0 +1,24 @@
+﻿function init_Table() {
+    if (!$.fn.DataTable.isDataTable('#apptDataTable')) {
+        $('#apptDataTable').dataTable({
+            columns: [
+                { "data": "id", "visible": false },
+                { "data": "日期" },
+                { "data": "時段" },
+                { "data": "科別" },
+                { "data": "醫師名稱" },
+                { "data": "診間" },
+                { "data": "診號" },
+                { "data": "看診狀態" }
+            ],
+            fixedHeader: {
+                header: true
+            },
+            language: {
+                url: "https://cdn.datatables.net/plug-ins/1.13.7/i18n/zh-HANT.json"
+            },
+            order: [[1, 'asc']]
+        });
+    }
+}
+init_Table()
