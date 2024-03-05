ce46c43360cb73b8244968d7039b2f60d4e6baff
NickInTheSandBox <154652330+NickInTheSandBox@users.noreply.github.com>
2024-03-05 15:32:10 +0800

    使用DataTables套件塞資料會壞掉。

--- ClinicWeb/wwwroot/js/memback/MBRecordInfo/DataGet.js
+++ ClinicWeb/wwwroot/js/memback/MBRecordInfo/DataGet.js
@@ -1,26 +1,9 @@
 ﻿//獲得主要病歷表資料
 var CASE_ID = 1;
 (() => {
-    const data = getRecord(CASE_ID);
-    console.log(data);
-    $('#recordDataTable').DataTable({
-        columns: [
-            { title: "看診紀錄ID", data: "recordID", visible: false },
-            { title: "血壓", data: "bloodPresure" },
-            { title: "脈搏", data: "pulse" },
-            { title: "體溫", data: "bodyTemparture" },
-            { title: "主述", data: "chiefComplaint" },
-            { title: "醫囑", data: "disposal" },
-            { title: "處方", data: "prescribe" },
-        ],
-        fixedHeader: {
-            header: true
-        },
-        language: {
-            url: "https://cdn.datatables.net/plug-ins/1.13.7/i18n/zh-HANT.json"
-        }
-    });
-});
+    getRecord(CASE_ID);
+    getReport(CASE_ID);
+})();
 async function getCase(id) {
     const response = await fetch(`/ClinicRoomSys/Cases/GM/${id}`, { method: "POST" })
     const data = await response.json();
@@ -48,21 +31,7 @@ async function getCase(id) {
     });
     // $('#recordDataTable').DataTable().clear();
     // $('#recordDataTable').DataTable().rows.add(record).draw();
-    $('#reportDataTable').DataTable({
-        columns: [
-            { title: "報告ID", data: "reportID", visible: false },
-            { title: "檢查名稱", data: "testName" },
-            { title: "檢查日期", data: "testDate" },
-            { title: "報告日期", data: "reportDate" },
-            { title: "結果", data: "result" },
-        ],
-        fixedHeader: {
-            header: true
-        },
-        language: {
-            url: "https://cdn.datatables.net/plug-ins/1.13.7/i18n/zh-HANT.json"
-        }
-    });
+  
     $('#prescriptionDataTable').DataTable({
         columns: [
             { title: "處方ID", data: "prescriptionID", visible: false },
@@ -84,16 +53,48 @@ async function getCase(id) {
 
 //獲得看診紀錄資料
 async function getRecord(id) {
-    const response = await fetch(`/ClinicRoomSys/Cases/GRD/${id}`, { method: "POST" })
+    const response = await fetch(`/MBRecordInfo/GRD/${id}`, { method: "POST" })
     const data = await response.json();
     // return data;
+    $('#recordDataTable').DataTable({
+        columns: [
+            { title: "看診紀錄ID", data: "recordID", visible: false },
+            { title: "血壓", data: "bloodPresure" },
+            { title: "脈搏", data: "pulse" },
+            { title: "體溫", data: "bodyTemparture" },
+            { title: "主述", data: "chiefComplaint" },
+            { title: "醫囑", data: "disposal" },
+            { title: "處方", data: "prescribe" },
+        ],
+        fixedHeader: {
+            header: true
+        },
+        language: {
+            url: "https://cdn.datatables.net/plug-ins/1.13.7/i18n/zh-HANT.json"
+        }
+    });
     $('#recordDataTable').DataTable().rows.add(data).draw();
 }
 //獲得檢查報告資料
 async function getReport(id) {
-    const response = await fetch(`/ClinicRoomSys/Cases/GRT/${id}`, { method: "POST" })
+    const response = await fetch(`/MBRecordInfo/GRT/${id}`, { method: "POST" })
     const data = await response.json();
     //return data;
+    $('#reportDataTable').DataTable({
+        columns: [
+            { title: "報告ID", data: "reportID", visible: false },
+            { title: "檢查名稱", data: "testName" },
+            { title: "檢查日期", data: "testDate" },
+            { title: "報告日期", data: "reportDate" },
+            { title: "結果", data: "result" },
+        ],
+        fixedHeader: {
+            header: true
+        },
+        language: {
+            url: "https://cdn.datatables.net/plug-ins/1.13.7/i18n/zh-HANT.json"
+        }
+    });
     $('#reportDataTable').DataTable().rows.add(data).draw();
 }
 //獲得處方資料
