4bd34326c19a41e625f242a9f418801454da3d7b
Hakurou0116 <jojo41525@gmail.com>
2024-03-06 17:07:06 +0800

    03065資料秀出有問題先註解

--- ClinicWeb/wwwroot/js/memback/MBSatisfaction/MBSatisfaction.js
+++ ClinicWeb/wwwroot/js/memback/MBSatisfaction/MBSatisfaction.js
@@ -1,30 +1,30 @@
 ﻿
 const table = document.querySelector('#datatable');
 
-function QueryMedicalRecords() {
-    $('#MedicalRecordsTable').dataTable({
-        ajax: {
-            type: 'POST',
-            url: (`/MBSatisfactionController/Get_MedicalRecords`),
-            dataSrc: function (json) { return json; }
-        },
-        destroy: true,
-        columns: [
-            // { "data": "id", "visible": false },
-            { "data": "caseid", "visible": false },
-            { "data": "mrid", "visible": false },
-            { "data": "看診日期" },
-            { "data": "醫師" },
-            { "data": "時段" },
-            { "data": "科別" },
-            { "data": "診間" }
-        ],
-        fixedHeader: {
-            header: true
-        },
-        order: [[2, 'asc']]
-    });
+//function QueryMedicalRecords() {
+//    $('#MedicalRecordsTable').dataTable({
+//        ajax: {
+//            type: 'POST',
+//            url: (`/MBSatisfaction/Get_MedicalRecords`),
+//            dataSrc: function (json) { return json; }
+//        },
+//        destroy: true,
+//        columns: [
+//            // { "data": "id", "visible": false },
+//            { "data": "caseid", "visible": false },
+//            { "data": "mrid", "visible": false },
+//            { "data": "看診日期" },
+//            { "data": "醫師" },
+//            { "data": "時段" },
+//            { "data": "科別" },
+//            { "data": "診間" }
+//        ],
+//        fixedHeader: {
+//            header: true
+//        },
+//        order: [[2, 'asc']]
+//    });
 
-}
+//}
 
-QueryMedicalRecords();
+/*QueryMedicalRecords();*/
