18bfe4958f0ad2b12c2efa307b59552e00ac58aa
NickChen <y42146988@gmail.com>
2024-03-20 20:23:11 +0800

    QRCODE打卡完成。

--- ClinicWeb/wwwroot/js/memback/MBRecordInfo/QrcodeScript.js
+++ ClinicWeb/wwwroot/js/memback/MBRecordInfo/QrcodeScript.js
@@ -28,10 +28,9 @@
 /*const formattedTime = currentTime.toLocaleTimeString();*/
 
 //上班打卡
-const message = "";
 const startbutton = document.getElementById('startbutton');
 startbutton.addEventListener('click', function () {
-    const qrCodeContent = document.getElementById('qrCodeContent');
+    /*const qrCodeContent = document.getElementById('qrCodeContent');*/
     const html5QrCodeon = new Html5Qrcode("cameraPreviewon");
     html5QrCodeon.start(
         { facingMode: "environment" },
@@ -40,7 +39,7 @@ startbutton.addEventListener('click', function () {
             qrbox: { width: 300, height: 300 }  // QR 大小
         },
         (qrCodeMessage) => {
-            qrCodeContent.textContent = "Scanned QR code: " + qrCodeMessage;
+            /*qrCodeContent.textContent = "Scanned QR code: " + qrCodeMessage;*/
             const message = qrCodeMessage;
             console.log(qrCodeMessage);
 			check(message);
@@ -55,7 +54,7 @@ startbutton.addEventListener('click', function () {
 //下班打卡
 const endbutton = document.getElementById('endbutton');
 endbutton.addEventListener('click', function () {
-    const qrCodeContent = document.getElementById('qrCodeContent');
+    /*const qrCodeContent = document.getElementById('qrCodeContent');*/
 
     const html5QrCodeoff = new Html5Qrcode("cameraPreviewoff");
 
@@ -66,44 +65,13 @@ endbutton.addEventListener('click', function () {
             qrbox: { width: 300, height: 300 }  // QR 大小
         },
         (qrCodeMessage) => {
-            qrCodeContent.textContent = "Scanned QR code: " + qrCodeMessage;
-			console.log(qrCodeMessage); // 打印扫描到的QR码值到控制台
-
-			e.preventDefault();
-
-			var currentDate = new Date();
-			var isOnTime = currentDate.getHours() > 17;
-			var requestData = {
-				"FEmployeeId": qrCodeMessage,
-				"FCheckOutTime": currentDate.toISOString(),
-				"FWorkDate": currentDate.toISOString(),
-				"fAttendanceCOS": isOnTime ? "正常下班" : "早退",
-			};
-
-			$.ajax({
-				type: "POST",
-				url: "/Fqrcode/CHeckOut",
-				contentType: "application/json",
-				data: JSON.stringify(requestData),
-				success: function (response) {
-					console.log("Success:", response);
-					Swal.fire({
-						title: "Good job!",
-						text: response,
-						icon: "success"
-					});
-				},
-				error: function (error) {
-					console.error("Error:", error.responseText);
-					Swal.fire({
-						icon: "error",
-						title: "Oops...",
-						text: error.responseText,
-					});
-				}
-			});
-
-            html5QrCodeoff.stop(); // 停止
+            /*qrCodeContent.textContent = "Scanned QR code: " + qrCodeMessage;*/
+			console.log(qrCodeMessage); 
+			const message = qrCodeMessage;
+			console.log(qrCodeMessage);
+			out(message);
+			html5QrCodeoff.stop(); // 停止
+			$('#getOffModal').modal('hide');
         },
         (errorMessage) => {
             console.error(errorMessage);
@@ -205,4 +173,39 @@ function check(empID) {
 			});
 		}
 	});
+}
+
+function out(empID) {
+	var currentDate = new Date();
+	var isOnTime = currentDate.getHours() > 17;
+	var requestData = {
+		"FEmployeeId": empID,
+		"FCheckOutTime": currentDate.toISOString(),
+		"FWorkDate": currentDate.toISOString(),
+		"fAttendanceCOS": isOnTime ? "正常下班" : "早退",
+	};
+
+	$.ajax({
+		type: "POST",
+		url: "/Fqrcode/CHeckOut",
+		contentType: "application/json",
+		data: JSON.stringify(requestData),
+		success: function (response) {
+			console.log("Success:", response);
+			Swal.fire({
+				title: "Good job!",
+				text: response,
+				icon: "success"
+			});
+		},
+		error: function (error) {
+			console.error("Error:", error.responseText);
+			Swal.fire({
+				icon: "error",
+				title: "Oops...",
+				text: error.responseText,
+			});
+		}
+	});
+
 }
