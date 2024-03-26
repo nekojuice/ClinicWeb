23cc0f72fcbc65586c2dcbeec0f223df56065cb7
nekojuice <joseapple7@gmail.com>
2024-03-26 20:34:29 +0800

    新增回真-首頁連結、流程圖

--- ClinicWeb/Controllers/HomeController.cs
+++ ClinicWeb/Controllers/HomeController.cs
@@ -28,5 +28,10 @@ namespace ClinicWeb.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult FlowChart() 
+        {
+            return View();
+        }
     }
 }
